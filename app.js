94f1d47ee01498b6d16bade458c008bce3ddc375
Hamza-Zaidi619 <117271536+Hamza-Zaidi619@users.noreply.github.com>
2023-01-31 21:26:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+// let mypromise= new Promise((resolve, reject) => {
+//     if(mypromise =)
+    
+// });
+
+// let mujtabaKaRishta=(mujtabaKaApnaGhar,mujtabaKiBike,mujtabaKiSalary)=>{
+//     return new Promise((han,na)=>{
+//         if(mujtabaKaApnaGhar==true &&
+//             mujtabaKiBike==true &&
+//             mujtabaKiSalary>80
+//             ){
+//                 han("PAkka")
+//             }else{
+//                 na("NO!")
+//             }
+//     });
+// };
+//  mujtabaKaRishta(true,true,90)
+
+// .then((res)=>{
+//     console.log(res)
+// })
+// .catch((err)=>{
+//     console.log(err)
+// })
+
+
+// var promise = new Promise((resolve, reject) => {
+//     var marks = prompt()
+//     if(marks==90){
+//         resolve("YOURE PASSED")
+//     }else{
+//         reject("YOUVE FAILED")
+//     }
+// })
+// promise.then((data)=>{
+//     console.log(data)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// })
+
+
